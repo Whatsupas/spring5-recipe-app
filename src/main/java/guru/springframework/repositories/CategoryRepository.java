492c21f70068694331a0b4f2757a6883b06d4260
RobertasTutlys <robertastut@gmail.com>
2020-04-21 22:17:28 +0200

    Added query methods into repositories

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Robertas Tutlys on 2020-04-21
  */
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repositories;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Robertas Tutlys on 2020-04-21
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String Description );
 }
